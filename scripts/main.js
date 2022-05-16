0ca2ae0a226a823ceecfbf7ca31eab8c169be14f
faustocalvinio <faustocalvino@outlook.com>
2022-05-16 12:44:40 +0200

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-console.log("hola")
+const peopleAmount=document.querySelector('#people-amount');
+const billAmount=document.querySelector('#bill-amount');
+const tipAmount=document.querySelector('.tip-per-person-amount')
+const totalPerPerson=document.querySelector('.total-per-person-amount')
+
+const calculateFunc=()=>{
+    if (peopleAmount.value>0){
+        tipAmount.innerHTML=`$${((billAmount.value*0.05)/peopleAmount.value).toFixed(2)}`
+        totalPerPerson.innerHTML=`$${(billAmount.value/peopleAmount.value).toFixed(2)}`
+    }
+}
+
+
+billAmount.addEventListener('change',calculateFunc)
+peopleAmount.addEventListener('change',calculateFunc)
